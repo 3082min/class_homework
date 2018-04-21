bc817e4d087869ccb5af13b6b62935913a038429
Min Young Shin <mshin@pc42.cs.ucdavis.edu>
2018-04-21 16:31:06 -0700

    added calculation class, need to figure out how to create print

--- homework1/get_fraction.cpp
+++ homework1/get_fraction.cpp
@@ -4,8 +4,38 @@ using namespace std;
 class Fraction
 {
 	public:
-	  void calculate(int a, int b, int c, int d) {num = a + c; denom = b + d;}
 	  void print(void) {cout << num << "/" << denom << endl;}
+	private:
+	  int num, denom;
+};
+
+class Calculation
+{
+	public:
+	  void add (int a, int b, int c, int d)
+		{
+			num = a + c;
+			denom = b * d;
+			f.print();
+		}
+	  void subtract (int a, int b, int c, int d)
+		{
+			num = a - c;
+			denom = b * d;
+			f.print();
+		}
+	  void multiply (int a, int b, int c, int d)
+		{
+			num = a * c;
+			denom = b * d;
+			f.print();
+		}
+	  void divide (int a, int b, int c, int d)
+		{
+			num = a * d;
+			denom = b * c;
+			f.print();
+		}
 	private:
 	  int num, denom;
 };
@@ -13,10 +43,14 @@ class Fraction
 int main()
 {
 	Fraction f;
+	Calculation c;
 	int n1, d1, n2, d2;
-	char slash;
+	char slash, sign;
 
-	cin >> n1 >> slash >> d1 >> n2 >> slash >> d2;
-	f.calculate(n1, d1, n2, d2);
-	f.print();
+	cin >> n1 >> slash >> d1 >> sign >> n2 >> slash >> d2;
+
+	c.subtract(n1,d1,n2,d2);
+	c.add(n1,d1,n2,d2);
+	c.multiply(n1,d1,n2,d2);
+	c.divide(n1,d1,n2,d2);
 }
